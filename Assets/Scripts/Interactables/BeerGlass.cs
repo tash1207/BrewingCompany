b68d475a3b062c2049efc12fe0a45391f0da298d
Tasha Hankewych <thankewych@gmail.com>
2025-04-28 17:23:23 -0500

    Add the beer glass objects and a script to spawn them on tables

---
+++ Assets/Scripts/Interactables/BeerGlass.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BeerGlass : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] SpriteRenderer beerFill;
+
+    [Header("Settings")]
+    [SerializeField] float beerDepletionRate = 10f;
+    [SerializeField] float beerAmountDeemedEmpty = 0.35f;
+
+    void Update()
+    {
+        if (beerFill != null && beerFill.size.y > 0)
+        {
+            beerFill.size = new Vector2(
+                beerFill.size.x,
+                Mathf.Clamp(beerFill.size.y - (beerDepletionRate / 100 * Time.deltaTime), 0, 1));
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] GameObject tablesParentObject;
+    [SerializeField] GameObject[] beerPrefabs;
+
+    [Header("Settings")]
+    [SerializeField] float beerSpawnRate = 3.5f;
+
+    private List<GameObject> tables = new List<GameObject>();
+    private float beerTimer;
+    
+    void Start()
+    {
+        foreach(Transform child in tablesParentObject.transform)
+        {
+            tables.Add(child.gameObject);
+        }
+
+        beerTimer = beerSpawnRate;
+    }
+
+    void Update()
+    {
+        beerTimer -= Time.deltaTime;
+
+        if (beerTimer < 0)
+        {
+            SpawnBeer();
+        }
+    }
+
+    void SpawnBeer()
+    {
+        int tableIndex = Random.Range(0, tables.Count);
+        GameObject table = tables[tableIndex];
+        // TODO: Consider object pooling.
+        GameObject newBeer = Instantiate(
+            beerPrefabs[Random.Range(0, beerPrefabs.Length)],
+            GetSpawnPosition(table, tableIndex == 0),
+            Quaternion.identity);
+        newBeer.transform.parent = table.transform;
+
+        beerTimer = beerSpawnRate;
+    }
+
+    private Vector2 GetSpawnPosition(GameObject table, bool isHorizontalTable)
+    {
+        // TODO: Create Table script that contains the table's bounds for spawning.
+        float xRange = isHorizontalTable ? 0.94f : 0.7f;
+        float yRange = isHorizontalTable ? 0.42f : 0.8f;
+
+        float randomX = Random.Range(-xRange, xRange);
+        float randomY = Random.Range(-yRange, yRange);
+        return new Vector2(
+            table.transform.position.x + randomX,
+            table.transform.position.y + randomY);
+    }
+}
