32bc9014f0686c13f3286539a7093297efa2b29f
Tasha Hankewych <thankewych@gmail.com>
2025-04-28 15:57:11 -0500

    Add a player object that can be moved using the new input system

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private Vector2 moveInput;
+    private float moveSpeed = 3.5f;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.velocity = moveInput * moveSpeed;
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+}
