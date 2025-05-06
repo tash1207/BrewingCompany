8ef75832134ce4f4765fdfc5463a3d812e0aa18b
Tasha Hankewych <thankewych@gmail.com>
2025-05-06 02:06:54 -0500

    Change the color of settings toggles when they are selected

--- Assets/Scripts/MainMenu/SettingItem.cs
+++ Assets/Scripts/MainMenu/SettingItem.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SettingItem : MonoBehaviour
+public class SettingItem : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
     public SettingsManager.SettingItem settingItem;
+
+    [Header("References")]
     [SerializeField] Toggle toggle;
+    [SerializeField] Image toggleBackground;
+
+    [Header("Settings")]
+    [SerializeField] Color selectedColor;
 
     public void SetToggle(bool value)
     {
         // We don't want the toggle sound to play when initializing the toggle value.
         toggle.SetIsOnWithoutNotify(value);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        toggleBackground.color = selectedColor;
+        toggle.graphic.color = selectedColor;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        toggleBackground.color = Color.white;
+        toggle.graphic.color = Color.white;
     }
 }
